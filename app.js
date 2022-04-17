99fdf88567de0a67601a3b4a17d427a1711d3c4a
Fatvm <aitorvt95@gmail.com>
2022-04-17 20:25:47 +0200

    Change build location

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/data', express.static('./blog_images'));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('../webpage/build'));
 app.use('/api', admin);
 
 module.exports = app;
