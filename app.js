e9017d1300393ed0a17ad5752cab025b2a013971
Fatvm <aitorvt95@gmail.com>
2022-04-17 19:57:31 +0200

    Server config + build data + admin post

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+var express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const _ = require('lodash');
-var url = require('url');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
+var admin = require('./router/admin');
 const app = express();
 
 app.use(fileUpload({
@@ -13,42 +13,11 @@ app.use(fileUpload({
 }));
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
-app.use(express.static('uploads'));
+app.use('/data', express.static('./blog_images'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api', admin);
 
-const port = process.env.PORT || 3000;
-
-
-app.post('/upload', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false
-            });
-        } else {
-            //Use the name of the input field to retrieve the uploaded file
-            let file = req.files.file;
-            
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            file.mv('./uploads/' + file.name);
-
-            //send response
-            res.send({
-                status: true,
-                url: url.format({
-                    protocol: req.protocol,
-                    host: req.get('host'),
-                    pathname: file.name})
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
+module.exports = app;
