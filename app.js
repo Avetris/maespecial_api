d93367435113e7091e7245b4ce9c2a398a09790a
Fatvm <aitorvt95@gmail.com>
2022-04-18 12:25:08 +0200

    reverting some changes to test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var admin = require('./router/admin');
-var anonymous = require('./router/anonymous');
+// var anonymous = require('./router/anonymous');
 const app = express();
 
 app.use(fileUpload({
@@ -14,13 +14,13 @@ app.use(fileUpload({
 }));
 
 
-// app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/data', express.static('../blog_images'));
-app.use(express.static('../webpage/build'));
+app.use('/data', express.static(process.env.IMAGEPATH || '../blog_images'));
+app.use(express.static(process.env.BUILDPATH || '../webpage/build'));
 app.use('/api/admin', admin);
-app.use('/api/anony', anonymous);
+// app.use('/api/anony', anonymous);
 
 module.exports = app;
