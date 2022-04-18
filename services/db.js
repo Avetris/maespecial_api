2b43a9dd05540c3f5c77711316f058560ffd6ef6
Fatvm <aitorvt95@gmail.com>
2022-04-18 13:19:50 +0200

    added logger

--- services/db.js
+++ services/db.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2/promise');
 
+const logger = require('../logger')
+
 async function query(sql, params) {
+  logger.debug("Start query")
   const connection = await mysql.createConnection({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBDATABASE
   });
+  logger.debug("Middle query ", connection)
   const [results, ] = await connection.execute(sql, params);
+  logger.debug("End query ", result)
 
   return results;
 }
