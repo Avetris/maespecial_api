1eb893fa857eae3ec33c22f8746efa40b12fbab1
Fatvm <aitorvt95@gmail.com>
2022-04-18 12:31:02 +0200

    added anonymous part

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var admin = require('./router/admin');
-// var anonymous = require('./router/anonymous');
+var anonymous = require('./router/anonymous');
 const app = express();
 
 app.use(fileUpload({
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/data', express.static(process.env.IMAGEPATH || '../blog_images'));
-app.use(express.static(process.env.BUILDPATH || '../webpage/build'));
+app.use(express.static(process.env.BUILDPATH || '../maespecial_webpage/build'));
 app.use('/api/admin', admin);
 // app.use('/api/anony', anonymous);
 
