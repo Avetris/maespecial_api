1e5a13190c9aaad78c0951143fae35107f16d391
Fatvm <aitorvt95@gmail.com>
2022-04-18 12:33:21 +0200

    added list per page as env variable

--- services/data_manager/post.js
+++ services/data_manager/post.js
@@ -1,9 +1,9 @@
 const db = require('../db');
 const helper = require('../helper');
-const config = require('../../config');
+// const config = require('../../config');
 
 async function getMultiple(page = 1){
-  const offset = helper.getOffset(page, config.listPerPage);
+  const offset = helper.getOffset(page, process.env.LISTPERPAGE || 10);
   const rows = await db.query(
     /*`SELECT id, name, released_year, githut_rank, pypl_rank, tiobe_rank 
     FROM programming_languages LIMIT ${offset},${config.listPerPage}`*/
