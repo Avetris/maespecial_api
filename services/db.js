5f19c4cb8c427d34557b19cdb4e9a5cd9f12f671
Fatvm <aitorvt95@gmail.com>
2022-04-18 13:23:14 +0200

    added log as info

--- services/db.js
+++ services/db.js
@@ -3,16 +3,16 @@ const mysql = require('mysql2/promise');
 const logger = require('../logger')
 
 async function query(sql, params) {
-  logger.debug("Start query")
+  logger.info("Start query")
   const connection = await mysql.createConnection({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBDATABASE
   });
-  logger.debug("Middle query ", connection)
+  logger.info("Middle query ", connection)
   const [results, ] = await connection.execute(sql, params);
-  logger.debug("End query ", result)
+  logger.info("End query ", result)
 
   return results;
 }
