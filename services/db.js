47da8fd7a84e354656bc0e6a1f0055167694a932
Avetris <aitorvt95@gmail.com>
2022-04-19 13:35:32 +0200

    change mysql connection

--- services/db.js
+++ services/db.js
@@ -1,17 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const logger = require('../logger')
 
 async function query(sql, params) {
   logger.info("Start query ", process.env.DBHOST, process.env.DBUSER, process.env.DBPASSWORD, process.env.DBDATABASE)
-  const connection = await mysql.createConnection({
+  logger.info(process.env.DBHOST)
+  const connection = mysql.createConnection({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBDATABASE
   });
+
   logger.info("Middle query ", connection)
-  const [results, ] = await connection.execute(sql, params);
+  const [results, ] = connection.execute(sql, params);
   logger.info("End query ", result)
 
   return results;
